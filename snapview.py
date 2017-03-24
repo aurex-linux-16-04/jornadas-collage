9c57a9a0bc2806918b6bad1fd55cb29e42873100
Luis Garcia Gisbert <luisgg@gmail.com>
2017-03-24 15:48:41 +0100

    Some improvements for snapview

--- snapview.py
+++ snapview.py
@@ -11,7 +11,10 @@ def usage(param=''):
 		print param
 
 	print 'Usage: '+sys.argv[0]+' [-t TIME] [-f] FILE'
-	sys.exit()
+
+def die(param=''):
+	usage(param)
+        sys.exit()
 
 def clear_tmpfiles():
     filelist = glob.glob(imagefile + "*.jpg")
@@ -23,46 +26,48 @@ if __name__ == '__main__':
 	
 	follow = False
 	timelapse = 5
+	title="snapview"
 
 	try:
 		opts, img_files = getopt.getopt(sys.argv[1:], "hft:", ["help", "follow", "time="])
 	except getopt.GetoptError:
-		usage()
+		die()
                 sys.exit(2)
 
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
-			usage()
+			die()
 		elif opt in ( "-f", "--follow"):
 			follow = True
 		elif opt in ( "-t", "--time"):
 			timelapse = int(arg)
 
 	if not img_files:
-		usage()
+		die()
 
-	img_file = img_files[0]
-	if not os.path.isfile(img_file):
-		usage(img_file+" not found")
-
-	print img_file
-	runShow = True
-	escShow = False
-
-	showtime = 0
+#	img_file = img_files[0]
 	(x, y) = (0, 0)
-        image_cv = cv.LoadImage(img_file)
-        cv.ShowImage(img_file, image_cv)
-	cv.MoveWindow(img_file, x, y)
+	cv.MoveWindow(title, x, y)
+	runShow = True
 	while runShow:
-        	image_cv = cv.LoadImage(img_file)
-        	cv.ShowImage(img_file, image_cv)
-		c = cv.WaitKey(timelapse*1000) % 256
-		if c == 27:
-			# ESC pressed. Finish the program
-			follow = False
+		for img_file in img_files:
+			if runShow:
+				if not os.path.isfile(img_file):
+					usage(img_file+" not found")
+				else:
+		
+					#print img_file
 
+				        image_cv = cv.LoadImage(img_file)
+					cv.ShowImage(title, image_cv)
+					c = cv.WaitKey(timelapse*1000) % 256
+					if c == 27:
+						# ESC pressed. Finish the program
+						follow = False
+						runShow = False
+	
 		runShow = follow
+		
 
-	cv.DestroyWindow(img_file)
+	cv.DestroyWindow(title)
 
